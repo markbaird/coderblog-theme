0cdfb4b13db3659c6c06abc10a2f991a7a68149b
Mark Baird <mark.r.baird@gmail.com>
2015-07-26 20:40:21 -0400

    Fixing page display

--- controllers/page.js
+++ controllers/page.js
@@ -15,130 +15,171 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-module.exports = function PageModule(pb) {
-    
+module.exports = function(pb) {
+
     //pb dependencies
-    var util  = pb.util;
-    var Index = require('./index.js')(pb);
+    var util = pb.util;
+    var ContentViewLoader = pb.ContentViewLoader;
 
     /**
-     * Loads a page
-     * @class PageController
+     * Loads a single article
+     * @class ArticleViewController
      * @constructor
+     * @extends BaseController
      */
-    function PageController(){}
-    util.inherits(PageController, Index);
+    function PageViewController(){}
+    util.inherits(PageViewController, pb.BaseController);
 
     /**
-     * Looks up a page and renders it
-     * @see BaseController#render
+     * @method init
+     * @param {Object} content
+     * @param {Function} cb
+     */
+    PageViewController.prototype.init = function(context, cb) {
+        var self = this;
+        var init = function(err) {
+            if (util.isError(err)) {
+                return cb(err);
+            }
+
+            //create the service
+            self.service = new pb.PageService(self.getServiceContext());
+
+            //create the loader context
+            var context     = self.getServiceContext();
+            context.service = self.service;
+            self.contentViewLoader = new pb.ContentViewLoader(context);
+            self.contentViewLoader.getDefaultTemplatePath = function() {
+                return 'page';
+            };
+
+            self.contentViewLoader.renderContent = function(content, options, cb) {
+                var self = this;
+
+                //set recurring params
+                if (util.isNullOrUndefined(options.contentIndex)) {
+                    options.contentIndex = 0;
+                }
+
+                var isPage           = this.service.getType() === 'page';
+                var showByLine       = this.contentSettings.display_bylines && !isPage;
+                var showTimestamp    = this.contentSettings.display_timestamp && !isPage;
+                var ats              = self.ts.getChildInstance();
+                self.ts.reprocess = false;
+                ats.registerLocal('article_permalink', function(flag, cb) {
+                    self.onContentPermalink(content, options, cb);
+                });
+                ats.registerLocal('article_headline', function(flag, cb) {
+                    self.onContentHeadline(content, options, cb);
+                });
+
+                ats.registerLocal('article_headline_nolink', content.headline);
+                ats.registerLocal('article_subheading', ContentViewLoader.valOrEmpty(content.subheading));
+                ats.registerLocal('article_subheading_display', ContentViewLoader.getDisplayAttr(content.subheading));
+                ats.registerLocal('article_id', content[pb.DAO.getIdField()] + '');
+                ats.registerLocal('article_index', options.contentIndex);
+                ats.registerLocal('article_timestamp', showTimestamp && content.timestamp ? content.timestamp : '');
+                ats.registerLocal('article_timestamp_display', ContentViewLoader.getDisplayAttr(showTimestamp));
+                ats.registerLocal('article_layout', new pb.TemplateValue(content.layout, false));
+                ats.registerLocal('article_url', content.url);
+                ats.registerLocal('display_byline', ContentViewLoader.getDisplayAttr(showByLine));
+                ats.registerLocal('author_photo', ContentViewLoader.valOrEmpty(content.author_photo));
+                ats.registerLocal('author_photo_display', ContentViewLoader.getDisplayAttr(content.author_photo));
+                ats.registerLocal('author_name', ContentViewLoader.valOrEmpty(content.author_name));
+                ats.registerLocal('author_position', ContentViewLoader.valOrEmpty(content.author_position));
+                ats.registerLocal('media_body_style', ContentViewLoader.valOrEmpty(content.media_body_style));
+                ats.registerLocal('comments', function(flag, cb) {
+                    if (isPage || !pb.ArticleService.allowComments(self.contentSettings, content)) {
+                        return cb(null, '');
+                    }
+
+                    var ts = ats.getChildInstance();
+                    self.renderComments(content, ts, function(err, comments) {
+                        cb(err, new pb.TemplateValue(comments, false));
+                    });
+                });
+                ats.load('elements/page', cb);
+
+                options.contentIndex++;
+            };
+
+            cb(null, true);
+        };
+        PageViewController.super_.prototype.init.apply(this, [context, init]);
+    };
+
+    /**
      * @method render
      * @param {Function} cb
      */
-    PageController.prototype.render = function(cb) {
+    PageViewController.prototype.render = function(cb) {
         var self    = this;
         var custUrl = this.pathVars.customUrl;
 
-        //check for object ID as the custom URL
-        var doRedirect = false;
-        var where      = null;
-        if(pb.validation.isIdStr(custUrl)) {
-            where = {_id: pb.DAO.getObjectID(custUrl)};
-            if (pb.log.isSilly()) {
-                pb.log.silly("ArticleController: The custom URL was not an object ID [%s].  Will now search url field. [%s]", custUrl, e.message);
+        //attempt to load object
+        var opts = {
+            render: true,
+            where: this.getWhereClause(custUrl)
+        };
+        this.service.getSingle(opts, function(err, content) {
+            if (util.isError(err)) {
+                return cb(err);
             }
-        }
-        else {
-            where = {url: custUrl};
-        }
+            else if (content == null) {
+                return self.reqHandler.serve404();
+            }
 
-        var dao = new pb.DAO();
-        dao.loadByValues(where, 'page', function(err, page) {
-            if (util.isError(err) || page == null) {
-                if (where.url) {
-                    self.reqHandler.serve404();
-                    return;
+            var options = {};
+            self.contentViewLoader.render([content], options, function(err, html) {
+                if (util.isError(err)) {
+                    return cb(err);
                 }
 
-                dao.loadByValues({url: custUrl}, 'page', function(err, page) {
-                    if (util.isError(err) || page == null) {
-                        self.reqHandler.serve404();
-                        return;
-                    }
-
-                    self.renderPage(page, cb);
-                });
-
-                return;
-            }
-
-            self.renderPage(page, cb);
-        });
-    };
-
-    PageController.prototype.renderContent = function(content, contentSettings, themeSettings, index, cb) {
-        var self = this;
-
-        var isPage           = content.object_type === 'page';
-        var showByLine       = contentSettings.display_bylines && !isPage;
-        var showTimestamp    = contentSettings.display_timestamp && !isPage;
-        var ats              = new pb.TemplateService(this.ls);
-        var contentUrlPrefix = isPage ? '/page/' : '/article/';
-        self.ts.reprocess = false;
-        ats.registerLocal('article_permalink', pb.UrlService.urlJoin(pb.config.siteRoot, contentUrlPrefix, content.url));
-        ats.registerLocal('article_headline', new pb.TemplateValue('<a href="' + pb.UrlService.urlJoin(contentUrlPrefix, content.url) + '">' + content.headline + '</a>', false));
-        ats.registerLocal('article_headline_nolink', content.headline);
-        ats.registerLocal('article_subheading', content.subheading ? content.subheading : '');
-        ats.registerLocal('article_subheading_display', content.subheading ? '' : 'display:none;');
-        ats.registerLocal('article_id', content[pb.DAO.getIdField()].toString());
-        ats.registerLocal('article_index', index);
-        ats.registerLocal('article_timestamp', showTimestamp && content.timestamp ? content.timestamp : '');
-        ats.registerLocal('article_timestamp_display', showTimestamp ? '' : 'display:none;');
-        ats.registerLocal('article_layout', new pb.TemplateValue(content.layout, false));
-        ats.registerLocal('article_url', content.url);
-        ats.registerLocal('display_byline', showByLine ? '' : 'display:none;');
-        ats.registerLocal('author_photo', content.author_photo ? content.author_photo : '');
-        ats.registerLocal('author_photo_display', content.author_photo ? '' : 'display:none;');
-        ats.registerLocal('author_name', content.author_name ? content.author_name : '');
-        ats.registerLocal('media_body_style', content.media_body_style ? content.media_body_style : '');
-
-        ats.registerLocal('topics', function(flag, cb) {
-            self.getTopics(content, function(topics) {
-                var tasks = util.getTasks(topics, function(topics, i) {
-                    return function(callback) {
-                        self.renderTopic(topics[i], contentSettings, themeSettings, i, callback);
-                    };
-                });
-                async.parallel(tasks, function(err, result) {
-                    cb(err, new pb.TemplateValue(result.join(''), false));
-                });
+                var result = {
+                    content: html
+                };
+                cb(result);
             });
         });
-
-        ats.load('elements/page', cb);
-    };
-
-    PageController.prototype.renderPage = function(page, cb) {
-        this.req.pencilblue_page = page._id.toString();
-        this.page = page;
-        this.setPageName(page.name);
-        PageController.super_.prototype.render.apply(this, [cb]);
-    };
-
-    PageController.prototype.getTemplate = function(content, cb) {
-        cb(null, 'page');
-        return;
     };
 
     /**
-     * Retrieves the name of the page.  The page's headhile
-     *
+     * Builds out the where clause for finding the article to render.  Because
+     * MongoDB has an object ID represented by 12 characters we must account
+     * for this condition by building a where clause with an "or" condition.
+     * Otherwise we will only query on the url key
+     * @method getWhereClause
+     * @param {String} custUrl Represents the article's ID or its slug
+     * @return {Object} An object representing the where clause to use in the
+     * query to locate the article
      */
-    PageController.prototype.getPageTitle = function() {
-        return this.page.headline;
+    PageViewController.prototype.getWhereClause = function(custUrl) {
+
+        //put a check to look up by ID *FIRST*
+        var conditions = [];
+        if(pb.validation.isIdStr(custUrl, true)) {
+            conditions.push(pb.DAO.getIdWhere(custUrl));
+        }
+
+        //put a check to look up by URL
+        conditions.push({
+            url: custUrl
+        });
+
+        //check for object ID as the custom URL
+        var where;
+        if (conditions.length > 1) {
+            where = {
+                $or: conditions
+            };
+        }
+        else {
+            where = conditions[0];
+        }
+        return where;
     };
 
-    PageController.getRoutes = function(cb) {
+    PageViewController.getRoutes = function(cb) {
         var routes = [{
             method: 'get',
             path: "/page/:customUrl",
@@ -149,5 +190,5 @@ module.exports = function PageModule(pb) {
     };
 
     //exports
-    return PageController;
+    return PageViewController;
 };
