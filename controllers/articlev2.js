6e51a03cf399d28032f17ef7a7deea6adbc671f3
Mark Baird <mark.r.baird@gmail.com>
2015-07-26 20:28:20 -0400

    Working on new article controller

--- controllers/articlev2.js
+++ controllers/articlev2.js
@@ -19,6 +19,8 @@ module.exports = function(pb) {
 
   //pb dependencies
   var util = pb.util;
+  var async = require('async');
+  var ContentViewLoader = pb.ContentViewLoader;
 
   /**
    * Loads a single article
@@ -47,13 +49,65 @@ module.exports = function(pb) {
       //create the loader context
       var context     = self.getServiceContext();
       context.service = self.service;
+
       self.contentViewLoader = new pb.ContentViewLoader(context);
       self.contentViewLoader.getDefaultTemplatePath = function() {
         return 'article';
       };
 
+      self.contentViewLoader.renderContent = function(content, options, cb) {
+        var self = this;
+
+        //set recurring params
+        if (util.isNullOrUndefined(options.contentIndex)) {
+          options.contentIndex = 0;
+        }
+
+        var isPage           = this.service.getType() === 'page';
+        var showByLine       = this.contentSettings.display_bylines && !isPage;
+        var showTimestamp    = this.contentSettings.display_timestamp && !isPage;
+        var ats              = self.ts.getChildInstance();
+        self.ts.reprocess = false;
+        ats.registerLocal('article_permalink', function(flag, cb) {
+          self.onContentPermalink(content, options, cb);
+        });
+        ats.registerLocal('article_headline', function(flag, cb) {
+          self.onContentHeadline(content, options, cb);
+        });
+
+        ats.registerLocal('article_headline_nolink', content.headline);
+        ats.registerLocal('article_subheading', ContentViewLoader.valOrEmpty(content.subheading));
+        ats.registerLocal('article_subheading_display', ContentViewLoader.getDisplayAttr(content.subheading));
+        ats.registerLocal('article_id', content[pb.DAO.getIdField()] + '');
+        ats.registerLocal('article_index', options.contentIndex);
+        ats.registerLocal('article_timestamp', showTimestamp && content.timestamp ? content.timestamp : '');
+        ats.registerLocal('article_timestamp_display', ContentViewLoader.getDisplayAttr(showTimestamp));
+        ats.registerLocal('article_layout', new pb.TemplateValue(content.layout, false));
+        ats.registerLocal('article_url', content.url);
+        ats.registerLocal('display_byline', ContentViewLoader.getDisplayAttr(showByLine));
+        ats.registerLocal('author_photo', ContentViewLoader.valOrEmpty(content.author_photo));
+        ats.registerLocal('author_photo_display', ContentViewLoader.getDisplayAttr(content.author_photo));
+        ats.registerLocal('author_name', ContentViewLoader.valOrEmpty(content.author_name));
+        ats.registerLocal('author_position', ContentViewLoader.valOrEmpty(content.author_position));
+        ats.registerLocal('media_body_style', ContentViewLoader.valOrEmpty(content.media_body_style));
+        ats.registerLocal('comments', function(flag, cb) {
+          if (isPage || !pb.ArticleService.allowComments(self.contentSettings, content)) {
+            return cb(null, '');
+          }
+
+          var ts = ats.getChildInstance();
+          self.renderComments(content, ts, function(err, comments) {
+            cb(err, new pb.TemplateValue(comments, false));
+          });
+        });
+        ats.load('elements/article_full', cb);
+
+        options.contentIndex++;
+      };
+
       cb(null, true);
     };
+
     ArticleViewController.super_.prototype.init.apply(this, [context, init]);
   };
 
@@ -86,6 +140,19 @@ module.exports = function(pb) {
       self.ts.registerLocal('article_published', article.publish_date ? article.publish_date.toISOString() : '');
       self.ts.registerLocal('article_modified', article.last_modified ? article.last_modified.toISOString() : '');
 
+      self.ts.registerLocal('topics', function(flag, cb) {
+        self.getTopics(article, function(topics) {
+          var tasks = util.getTasks(topics, function(topics, i) {
+            return function(callback) {
+              self.renderTopic(topics[i], i, callback);
+            };
+          });
+          async.parallel(tasks, function(err, result) {
+            cb(err, new pb.TemplateValue(result.join(''), false));
+          });
+        });
+      });
+
       var options = {};
       self.contentViewLoader.render([article], options, function(err, html) {
         if (util.isError(err)) {
@@ -97,6 +164,28 @@ module.exports = function(pb) {
         };
         cb(result);
       });
+    });
+  };
+
+  ArticleViewController.prototype.renderTopic = function(topic, index, cb) {
+    var ats = new pb.TemplateService(this.ls);
+    ats.registerLocal('topic_name', topic.name);
+    ats.registerLocal('topic_name_encoded', encodeURIComponent(topic.name));
+    ats.load('elements/topic', cb);
+  };
+
+  ArticleViewController.prototype.getTopics = function(article, cb) {
+    var topics = article.article_topics;
+
+    pb.log.info("TOPICS: ", article)
+
+    var dao = new pb.DAO();
+    var opts = {
+      where: pb.DAO.getIdInWhere(topics),
+      order: {name: 1}
+    };
+    dao.q('topic', opts, function(err, topicObjects) {
+      cb(topicObjects);
     });
   };
 
